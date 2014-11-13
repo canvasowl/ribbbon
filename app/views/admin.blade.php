2148e776e5d3ed61eb5fcd91c7edf446177b1f7c
Jefry <jefrycruz88@gmail.com>
2014-11-13 00:01:08 -0500

    Created basic empty admin template

---
+++ app/views/admin.blade.php
@@ -0,0 +1,16 @@
+<?php # /app/views/clients/index.blade.php  ?>
+@extends('templates/hud_master')
+
+@section('content')
+
+<div class="row">
+	<div class="col-xs-12">
+		<div class="app-wrapper">
+
+        </div>
+	</div>
+</div>
+
+@stop()
+
+
