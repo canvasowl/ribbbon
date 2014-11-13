8ee5cdc0f49972bb91fbd0de4180849269d3d8e6
Jefry <jefrycruz88@gmail.com>
2014-11-13 00:07:26 -0500

    Created Admin routes with passed filters

--- app/routes.php
+++ app/routes.php
@@ -9,33 +9,40 @@ Route::get('/', function()
 	}	
 });
 
-Route::get('/register', function(){ return View::make('register'); });
-Route::get('/signin', function(){ return View::make('signin'); });
+Route::get('register', function(){ return View::make('register'); });
+Route::get('signin', function(){ return View::make('signin'); });
+Route::get('learn', function(){ return View::make('learn'); });
 
-Route::resource('/users', 'UsersController');
-Route::post('/login', 'UsersController@login');
-Route::post('/make', 'UsersController@register');
-Route::get('/logout', 'UsersController@logout');
-Route::post('/request', 'UsersController@request');
-Route::get('/learn', function(){ return View::make('learn'); });
+//----------------- User routes
+Route::resource('users', 'UsersController');
+Route::post('login', 'UsersController@login');
+Route::post('make', 'UsersController@register');
+Route::get('logout', 'UsersController@logout');
+Route::post('request', 'UsersController@request');
+Route::post('resetPassword/{id}','UsersController@resetPassword');
 
-// The user needs to be logged in for these routes to work
+//----------------- Admin routes
+Route::group(array('before' => 'admin'), function()
+{	
+	Route::get('confirm','AdminController@confirm');
+	Route::get('invite','AdminController@invite');
+});
+
+//----------------- Auth routes
 Route::group(array('before' => 'auth'), function()
 {	
 	Route::resource('clients', 'ClientsController');
 	Route::resource('projects', 'ProjectsController');
 	Route::resource('tasks', 'TasksController');
 	
-	Route::get('/hud', array('as' => 'hud', function(){
+	Route::get('hud', array('as' => 'hud', function(){
 		return View::make('hud');
 	}));
 
-	Route::get('/profile', 'UsersController@index');
+	Route::get('profile', 'UsersController@index');
 });
 
-// User routes
-Route::post('/resetPassword/{id}','UsersController@resetPassword');
-
-// Route::get('mail',function(){	
-// 	return "email sent";
+// Route::get('mail',function(){			
+// 	sendBetaFollowUpMail("jefrycruz88@gmail.com");
+// 	return "mail sent";
 // });
