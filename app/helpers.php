7b8865cb0c83d87a88f4d51cb357d6c2d08d1a44
Jefry <jefrycruz88@gmail.com>
2014-11-13 00:06:01 -0500

    Created more email templates and helper functions

--- app/helpers.php
+++ app/helpers.php
@@ -7,14 +7,40 @@
 		MAIL FUNCTIONS
 ********************************/
 
-/** Sends a welcome email to the user */
-function sendWelcomeMail(){
-	$data = array( 'to' => Auth::user()->email);
+/** Sends a follow up email when the user signs up for beta */
+function sendBetaFollowUpMail($email){
+	$data = array('to' => $email);
 	
-	Mail::send('emails.welcome', array(), function($message) use ($data){
-    	$message->to($data['to'], 'Full Name')->subject('Welcome to Ribbbon');
+	Mail::send('emails.beta.follow', array(), function($message) use ($data) {
+		$message->to($data['to'], '')->subject('Inviation Request Confirmation');
+	});
+}
+
+/** Sends beta invitation **/
+function sendBetaInviteEmail($email, $name){
+	$data = array( 
+			'to' 	=> $email,
+			'name' 	=> $name
+			);
+	
+	Mail::send('emails.beta.invite', array('name' => $name), function($message) use ($data){
+    	$message->to($data['to'], $data['name'] )->subject('Ribbbon invitation');
 	});		
 }
 
-/** Sends the beta register email */
-// function sendBetaRegisterMail($to = "jefrycruz88@gmail.com"){ }
+/** Sends a welcome email to the user */
+function sendWelcomeMail(){
+	$data = array( 
+			'to' 	=> Auth::user()->email,
+			'name' 	=> Auth::user()->full_name
+			);
+	
+	Mail::send('emails.welcome', array('name' => $data['name'] ), function($message) use ($data){
+    	$message->to($data['to'], $data['name'] )->subject('Welcome to Ribbbon');
+	});		
+}
+
+/** Sends password changed email **/
+
+/** Sends account deletion email **/
+
