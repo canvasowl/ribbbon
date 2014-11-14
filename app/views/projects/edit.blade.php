487ca3af7ad0176bd01be4b21d57006122ff544b
Jefry <jefrycruz88@gmail.com>
2014-11-13 21:59:03 -0500

    Touched up the project edit view

--- app/views/projects/edit.blade.php
+++ app/views/projects/edit.blade.php
@@ -9,25 +9,23 @@
 		<div class="app-wrapper">        
             
             <div>
-                <center><h2 class="no-margin-top app-wrapper-title">{{ $project->name }}</h2></center>                                                   
-                <hr>
+                <center><h2 class="no-margin-top app-wrapper-title">{{ $project->name }}</h2></center><hr>
             </div>
-            
-            <h4>Information</h4>
-            <section class="info">
-	            {{ Form::model($project, array('method' => 'PATCH', 'route' => array('projects.update', $project->id))) }}	            	
-	            	<div class="col-xs-12 no-side-padding small-padding-right">
-		            	<div class="form-group">
-		            		<label>Name</label>{{ Form::text('name', null, array('class' => 'form-control', 'placeholder' => 'Name'))}}	
-		            	</div>	            		
-	            	</div>
-	            	<hr>
+                                    
+            {{ Form::model($project, array('method' => 'PATCH', 'route' => array('projects.update', $project->id))) }}	            	
+            	<div class="col-xs-12">	            		
 	            	<div class="form-group">
-						{{ Form::submit('Save', array('class' => 'btn btn-default pull-right' )); }}	            								
-	            	</div>	            	
-	            	<div class="clearfix"></div>
-	            {{ Form::close() }}	                        	
-            </section>
+	            		<label>Name</label>{{ Form::text('name', null, array('class' => 'form-control', 'placeholder' => 'Name'))}}	
+	            	</div>	            		
+            	</div>
+            	<hr>
+            	<div class="col-xs-12">
+	            	<div class="form-group">
+						{{ Form::submit('Save', array('class' => 'btn btn-default pull-right' )); }}	         			
+	            	</div>
+	            </div>	            	
+            	<div class="clearfix"></div>
+            {{ Form::close() }}	                        	            
 			     
 		</div>
 	</div>
