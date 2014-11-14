de583343b05455da192106ce57f57968a560ea67
Jefry <jefrycruz88@gmail.com>
2014-11-13 22:00:05 -0500

    Added project info pills

--- app/views/projects/show.blade.php
+++ app/views/projects/show.blade.php
@@ -19,6 +19,24 @@
 
             <div class="row">            	            
 	            <div class="col-xs-12 col-md-8">
+				  	
+				  	<!-- info pills -->
+				  	<ul class="list-inline">
+				  		@if ($project->production)
+				  			<li><a class="pill pill-primary" target="_blank" href="{{ $project->production }}">Production</a></li>
+				  		@endif
+				  		@if ($project->stage)
+				  			<li><a class="pill pill-primary" target="_blank" href="{{ $production->stage">Staging</a></li>
+				  		@endif
+				  		@if ($project->dev)
+				  			<li><a class="pill pill-primary" target="_blank" href="{{ $production->dev">Development</a></li>
+				  		@endif
+				  		@if ($project->github)
+				  			<li><a class="pill pill-dark" target="_blank" href="{{ $project->github"><i class="fa fa-github fa-lg"> Github</a></li>
+				  		@endif
+				  	</ul><br>
+				  	<!-- info pills -->
+
 					<!-- Nav tabs -->
 					<ul class="nav nav-tabs" role="tablist">
 					  <li class="active"><a href="#tasks" role="tab" data-toggle="tab">Remaining Tasks <i class="fa fa-tasks"></i></a></li>
@@ -123,6 +141,29 @@
 						  		</table>
 					  		@endif
 					  	</section>					  	
+					  </div>
+
+					  <!-- CREDENTIALS -->
+					  <div class="tab-pane" id="credentials">
+					  	<p class="pull-right"></p>
+					  	<div class="clearfix"></div>
+
+					  	<!-- server panel -->
+						<div class="panel panel-default">
+						  <div class="panel-heading">FTP & SSH Accounts</div>
+						  <div class="panel-body">					    
+			            	<p class="dimmed">For the weight, enter a number from 1-3, the higher the number the harder the task.</p>
+						  </div>	         	
+			            </div>
+
+					  	<!-- other panel -->
+						<div class="panel panel-default">
+						  <div class="panel-heading">Other Credentials</div>
+						  <div class="panel-body">					    
+			            	<p class="dimmed">For the weight, enter a number from 1-3, the higher the number the harder the task.</p>
+						  </div>	         	
+			            </div>
+
 					  </div>
 
 					</div>
