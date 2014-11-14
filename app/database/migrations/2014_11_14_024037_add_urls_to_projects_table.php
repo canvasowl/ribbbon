290dc7cf792e92f9a0a294673f2f8984d16ef93e
Jefry <jefrycruz88@gmail.com>
2014-11-13 21:59:30 -0500

    Added project urls migration file

---
+++ app/database/migrations/2014_11_14_024037_add_urls_to_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUrlsToProjectsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('projects', function(Blueprint $table)
+		{
+			$table->string('production');
+			$table->string('stage');
+			$table->string('dev');
+			$table->string('github');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('projects', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
