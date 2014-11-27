63335c17e07f2091b6fe902ff051d71338cc8a32
Jefry <jefrycruz88@gmail.com>
2014-11-27 01:03:21 -0500

    Changed placeholder for login password

--- app/views/signin.blade.php
+++ app/views/signin.blade.php
@@ -10,7 +10,7 @@
 				{{ Form::text( 'email', null, array('class' => 'form-control', "placeholder" => "email","autofocus" => "true" )) }}	
 			</div>
 			<div class="form-group">
-				{{ Form::password( 'password', array('class' => 'form-control', "placeholder" => "placeholder" )) }}	
+				{{ Form::password( 'password', array('class' => 'form-control', "placeholder" => "password" )) }}	
 			</div>
 			<div class="form-group">
 				{{ Form::submit( 'Login', array('class' => 'btn btn-rival pull-right')) }}
