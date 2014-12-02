cc7b6065d3a79f803f3fbf0f5d7b4e8ace3b5707
Jefry <jefrycruz88@gmail.com>
2014-12-01 23:09:00 -0500

    Added new Hud styles for latest tasks and latest projects panels

--- app/views/hud.blade.php
+++ app/views/hud.blade.php
@@ -30,4 +30,37 @@
 		</div>	
 	</div>
 
+	<div class="row">
+		<!-- Latest tasks -->
+		<div class="col-xs-12 col-md-6">
+			<div class="panel panel-tasks">
+			  <div class="panel-heading">
+			    <h3 class="panel-title">Latest Tasks</h3>
+			  </div>
+			  <div class="panel-body">
+			    <p><a href="">Lorem ipsum</a> <span class="level level-medium pull-right">1</span></p>
+			    <p>Lorem ipsum <span class="level level-medium pull-right">1</span></p>
+			    <p>Lorem ipsum <span class="level level-medium pull-right">1</span></p>
+			    <p>Lorem ipsum <span class="level level-medium pull-right">1</span></p>
+			    <p>Lorem ipsum <span class="level level-medium pull-right">1</span></p>
+			  </div>
+			</div>			
+		</div>
+		<!-- Latest projects -->
+		<div class="col-xs-12 col-md-6">
+			<div class="panel panel-projects">
+			  <div class="panel-heading">
+			    <h3 class="panel-title">Latest Projects</h3>
+			  </div>
+			  <div class="panel-body">
+			    <p>Lorem ipsum <span class="level level-medium pull-right">1</span></p>
+			    <p>Lorem ipsum <span class="level level-medium pull-right">1</span></p>
+			    <p>Lorem ipsum <span class="level level-medium pull-right">1</span></p>
+			    <p>Lorem ipsum <span class="level level-medium pull-right">1</span></p>
+			    <p>Lorem ipsum <span class="level level-medium pull-right">1</span></p>
+			  </div>
+			</div>			
+		</div>
+	</div>
+
 @stop()
