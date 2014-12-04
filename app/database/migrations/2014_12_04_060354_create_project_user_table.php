4217715761ec616f6978dca1207c838d61afc911
canvasowl <jefrycruz88@gmail.com>
2014-12-04 13:02:10 -0500

    Added new Pivot migrations file

---
+++ app/database/migrations/2014_12_04_060354_create_project_user_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateProjectUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('project_user', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('project_id')->unsigned()->index();
+			$table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+			$table->integer('user_id')->unsigned()->index();
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('project_user');
+	}
+
+}
