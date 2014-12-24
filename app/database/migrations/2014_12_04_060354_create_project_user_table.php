ce24fcf954cc10a4bad2e6dea84996277fc175c5
canvasowl <jefrycruz88@gmail.com>
2014-12-23 23:10:10 -0500

    Updated table name

--- app/database/migrations/2014_12_04_060354_create_project_user_table.php
+++ app/database/migrations/2014_12_04_060354_create_project_user_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateProjectUsersTable extends Migration {
+class CreateProjectUserTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,7 @@ class CreateProjectUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('projectusers', function(Blueprint $table)
+		Schema::create('project_user', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->integer('project_id')->unsigned()->index();
@@ -31,7 +31,7 @@ class CreateProjectUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('projectusers');
+		Schema::drop('project_user');
 	}
 
 }
