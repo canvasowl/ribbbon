b7a2058071eb739352da52d855a9f8c32d3bb347
canvasowl <jefrycruz88@gmail.com>
2014-12-30 21:12:34 -0500

    Removed name signiture

--- app/views/emails/layouts/masterNoSigniture.blade.php
+++ app/views/emails/layouts/masterNoSigniture.blade.php
@@ -357,7 +357,7 @@
                                                     <h3 style="color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; text-align: left; line-height: 1.3; word-break: normal; font-size: 40px; margin: 0; padding: 0;" align="left">Hey, @yield('name')</h3>
                                                     <p style="color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; text-align: left; line-height: 19px; font-size: 14px; margin: 0 0 10px; padding: 0;" align="left">@yield('intro')</p>
                                                     <p style="color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; text-align: left; line-height: 19px; font-size: 14px; margin: 0 0 10px; padding: 0;" align="left">@yield('content')</p>
-                                                    
+
                                                 </td>
                                                 <td class="expander" style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; vertical-align: top; text-align: left; visibility: hidden; width: 0px; color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; line-height: 19px; font-size: 14px; margin: 0; padding: 0;" align="left" valign="top"></td>
                                             </tr></table></td>
