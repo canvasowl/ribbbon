e81a3d0534b303982de43b75a01702a31277e7a7
canvasowl <jefrycruz88@gmail.com>
2015-01-02 01:12:15 -0500

    Added member and owner check to manage tab

--- app/views/projects/partials/sidebar.blade.php
+++ app/views/projects/partials/sidebar.blade.php
@@ -2,7 +2,9 @@
 <ul class="nav nav-pills nav-stacked">
   <li><a href="/projects/{{ $project->id }}"><i class="fa fa-tasks"></i> Tasks</a></li>
   <li><a href="/projects/{{ $project->id }}/credentials"><i class="fa fa-key"></i> Credentials</a></li>
-  <li><a href="/projects/{{ $project->id }}/manage"><i class="fa fa-cog"></i> Manage</a></li>
+  @if( isOwner($project->id) == true )
+    <li><a href="/projects/{{ $project->id }}/manage"><i class="fa fa-cog"></i> Manage</a></li>
+  @endif
 </ul>
 <!-- links -->
 
