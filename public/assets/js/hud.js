24eddee85290c3d46d8e8a756c4d43772fde2e93
Jefry <jefrycruz88@gmail.com>
2015-01-15 22:03:50 -0500

    Hud api scripts will only run on / and /hud

--- public/assets/js/hud.js
+++ public/assets/js/hud.js
@@ -1,80 +1,85 @@
+var current_url = window.location.pathname;
+
 var userId;
 var incompleteCount;
 var completedCount;
 
-/**** AUTH ID */
-function getAuthId(){
-	return $.ajax({
-		    url:  '/api/0000000000/authId',
-		    type: 'GET',
-		    dataType: 'json',
-		});
-}
+if (current_url == "/hud" || current_url == "/") {
 
+		/**** AUTH ID */
+		function getAuthId(){
+			return $.ajax({
+				    url:  '/api/0000000000/authId',
+				    type: 'GET',
+				    dataType: 'json',
+				});
+		}
 
+		// Handle the getAutId data
+		function handleAuthIdData(data /* , textStatus, jqXHR */ ) {
+		    userId = data;
+		}
 
-// Handle the getAutId data
-function handleAuthIdData(data /* , textStatus, jqXHR */ ) {
-    userId = data;
-}
+		// Handle the incompleteCount
+		function handleIncompleteCount(data /* , textStatus, jqXHR */ ) {
+		    incompleteCount = data.length;
+		}
 
-// Handle the incompleteCount
-function handleIncompleteCount(data /* , textStatus, jqXHR */ ) {
-    incompleteCount = data.length;
-}
+		// Handle the completeCount
+		function handleCompleteCount(data /* , textStatus, jqXHR */ ) {
+		    completeCount = data.length;
+		}
 
-// Handle the completeCount
-function handleCompleteCount(data /* , textStatus, jqXHR */ ) {
-    completeCount = data.length;
-}
-
-getAuthId().done(handleAuthIdData).done(function(){
-	
-	/**** Incomplete Count */
-	function getIncompleteCount(){
-		return $.ajax({
-			    url:  '/api/0000000000/'+ userId +'/tasks/incomplete',
-			    type: 'GET',
-			    dataType: 'json',
-			});
-	}
-	
-	getIncompleteCount().done(handleIncompleteCount).done(function(){
-
+		getAuthId().done(handleAuthIdData).done(function(){
+			
 			/**** Incomplete Count */
-			function getCompleteCount(){
+			function getIncompleteCount(){
 				return $.ajax({
-					    url:  '/api/0000000000/'+ userId +'/tasks/complete',
+					    url:  '/api/0000000000/'+ userId +'/tasks/incomplete',
 					    type: 'GET',
 					    dataType: 'json',
 					});
 			}
+			
+			getIncompleteCount().done(handleIncompleteCount).done(function(){
 
-			getCompleteCount().done(handleCompleteCount).done(function(){
+					/**** Incomplete Count */
+					function getCompleteCount(){
+						return $.ajax({
+							    url:  '/api/0000000000/'+ userId +'/tasks/complete',
+							    type: 'GET',
+							    dataType: 'json',
+							});
+					}
 
-				// Now that we have completed counts and 
-				// incomplete count numbers
-				// lets create the doughnut chart.
+					getCompleteCount().done(handleCompleteCount).done(function(){
 
-				// Set the informatio for the graph
-				var doughnutData = [
-						{
-							value: incompleteCount,
-							color:"#B75DB6",
-							highlight: "#944B94",
-							label: "Incomplete Tasks"
-						},
-						{
-							value: completeCount,
-							color: "#40F4C4",
-							highlight: "#18FFC6",
-							label: "Completed Tasks"
-						}
-					];
+						// Now that we have completed counts and 
+						// incomplete count numbers
+						// lets create the doughnut chart.
 
-					// Create the graph
-					var ctx = document.getElementById("chart-area").getContext("2d");
-					window.myDoughnut = new Chart(ctx).Doughnut(doughnutData, {responsive : true});					
-			});
-	})
-});
+						// Set the informatio for the graph
+						var doughnutData = [
+								{
+									value: incompleteCount,
+									color:"#B75DB6",
+									highlight: "#944B94",
+									label: "Incomplete Tasks"
+								},
+								{
+									value: completeCount,
+									color: "#40F4C4",
+									highlight: "#18FFC6",
+									label: "Completed Tasks"
+								}
+							];
+
+							// Create the graph
+							var ctx = document.getElementById("chart-area").getContext("2d");
+							window.myDoughnut = new Chart(ctx).Doughnut(doughnutData, {responsive : true});					
+					});
+			})
+		});
+
+};
+
