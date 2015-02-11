db1bfe53e64b475a015a7ea9b5df5ee718f9933a
Jefry <jefrycruz88@gmail.com>
2015-02-10 23:38:55 -0500

    Made credentials wider, credentials section will hide if count = 0

--- app/views/projects/partials/credentials.blade.php
+++ app/views/projects/partials/credentials.blade.php
@@ -27,75 +27,79 @@
 
 
 <hr>
-<!-- server panel -->
-<div>
-  <div><strong>FTP & SSH Accounts</strong></div>
-  <div class="ftp-panel-body">
- 	<div class="row">
-     	@foreach ($credentials as $credential)
-     		@if ($credential->type == true)
-	     		<div class="col-xs-12 col-md-4 credential-wrap" id="credential-wrap-{{ $credential->id }}"><br>
-					<section class="info">
-						<h4>{{ $credential->name }}</h4	>
-						<p><strong>Hostname:</strong> {{ $credential->hostname }}</p>
-						<p><strong>Username:</strong> {{ $credential->username }}</p>
-						<p><strong>Password:</strong> {{ $credential->password }}</p>
-						<p><strong>Port:</strong> {{ $credential->port }}</p>
 
-						@if($owner_id == Auth::id())
-							<div class="actions">
-								<ul class="inline-list list-style-none">
-									<li>
-									{{ Form::open(array('action' => 'CredentialsController@destroy', 'method' => 'delete')) }}
-										<input type="hidden" name="id" value="{{ $credential->id }}">
-										<button title="delete" id="{{ $credential->id }}" type="submit" class="btn btn-default btn-delete"><i class="fa fa-trash"></i></button>
-									{{ Form::close() }}
-									</li>
-									<?php /*<li>
-										<button title="edit" class="btn btn-default"><a href=""><i class="fa fa-pencil"></i></a></button>
-									</li>*/ ?>
-									<div class="clearfix"></div>
-								</ul>
-							</div>
-						@endif
-					</section>
-	     		</div>
-     		@endif
-     	@endforeach
- 	</div>					     	
-  </div>	         	
-</div><br>
 
-<!-- other panel -->
-<div>
-  <div><strong>Other Credentials</strong></div>
-	  <div class="other-panel-body">
-	 	<div class="row">
-	     	@foreach ($credentials as $credential)
-	     		@if ($credential->type == false)
-		     		<div class="col-xs-12 col-md-4 credential-wrap" id="credential-wrap-{{ $credential->id }}"><br>
-						<section class="info">
-							<h4>{{ $credential->name }}</h4	>
-							<p><strong>Username:</strong> {{ $credential->username }}</p>
-							<p><strong>Password:</strong> {{ $credential->password }}</p>
+@if( count($credentials) > 0)
+    <!-- server panel -->
+    <div>
+      <div><strong>FTP & SSH Accounts</strong></div>
+      <div class="ftp-panel-body">
+        <div class="row">
+            @foreach ($credentials as $credential)
+                @if ($credential->type == true)
+                    <div class="col-xs-12 col-md-6 credential-wrap" id="credential-wrap-{{ $credential->id }}"><br>
+                        <section class="info">
+                            <h4>{{ $credential->name }}</h4	>
+                            <p><strong>Hostname:</strong> {{ $credential->hostname }}</p>
+                            <p><strong>Username:</strong> {{ $credential->username }}</p>
+                            <p><strong>Password:</strong> {{ $credential->password }}</p>
+                            <p><strong>Port:</strong> {{ $credential->port }}</p>
 
-							@if($owner_id == Auth::id())
-								<div class="actions">
-									<ul class="inline-list list-style-none">
-										<li>
-										{{ Form::open(array('action' => 'CredentialsController@destroy', 'method' => 'delete')) }}
-											<input type="hidden" name="id" value="{{ $credential->id }}">
-											<button title="delete" id="{{ $credential->id }}" type="submit" class="btn btn-default btn-delete"><i class="fa fa-trash"></i></button>
-										{{ Form::close() }}
-										</li>
-										<div class="clearfix"></div>
-									</ul>
-								</div>
-							@endif
-						</section>
-		     		</div>
-	     		@endif
-	     	@endforeach
-	 	</div>
-	  </div>	         	
-</div>
+                            @if($owner_id == Auth::id())
+                                <div class="actions">
+                                    <ul class="inline-list list-style-none">
+                                        <li>
+                                        {{ Form::open(array('action' => 'CredentialsController@destroy', 'method' => 'delete')) }}
+                                            <input type="hidden" name="id" value="{{ $credential->id }}">
+                                            <button title="delete" id="{{ $credential->id }}" type="submit" class="btn btn-default btn-delete"><i class="fa fa-trash"></i></button>
+                                        {{ Form::close() }}
+                                        </li>
+                                        <?php /*<li>
+                                            <button title="edit" class="btn btn-default"><a href=""><i class="fa fa-pencil"></i></a></button>
+                                        </li>*/ ?>
+                                        <div class="clearfix"></div>
+                                    </ul>
+                                </div>
+                            @endif
+                        </section>
+                    </div>
+                @endif
+            @endforeach
+        </div>
+      </div>
+    </div><br>
+
+    <!-- other panel -->
+    <div>
+      <div><strong>Other Credentials</strong></div>
+          <div class="other-panel-body">
+            <div class="row">
+                @foreach ($credentials as $credential)
+                    @if ($credential->type == false)
+                        <div class="col-xs-12 col-md-6 credential-wrap" id="credential-wrap-{{ $credential->id }}"><br>
+                            <section class="info">
+                                <h4>{{ $credential->name }}</h4	>
+                                <p><strong>Username:</strong> {{ $credential->username }}</p>
+                                <p><strong>Password:</strong> {{ $credential->password }}</p>
+
+                                @if($owner_id == Auth::id())
+                                    <div class="actions">
+                                        <ul class="inline-list list-style-none">
+                                            <li>
+                                            {{ Form::open(array('action' => 'CredentialsController@destroy', 'method' => 'delete')) }}
+                                                <input type="hidden" name="id" value="{{ $credential->id }}">
+                                                <button title="delete" id="{{ $credential->id }}" type="submit" class="btn btn-default btn-delete"><i class="fa fa-trash"></i></button>
+                                            {{ Form::close() }}
+                                            </li>
+                                            <div class="clearfix"></div>
+                                        </ul>
+                                    </div>
+                                @endif
+                            </section>
+                        </div>
+                    @endif
+                @endforeach
+            </div>
+          </div>
+    </div>
+@endif
