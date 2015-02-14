392bce4a989819d715ed8060be833c17f1c7c4ff
Jefry <jefrycruz88@gmail.com>
2015-02-14 13:12:04 -0500

    Added enviroment variables for DB credentials

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'rainbow',
-			'username'  => 'forge',
-			'password'  => 'pdeBrYfcUgRPVZYX2m8e',
+			'host'      => $_ENV['DB_HOST'],
+			'database'  => $_ENV['DB_NAME'],
+			'username'  => $_ENV['DB_USERNAME'],
+			'password'  => $_ENV['PASSWORD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
