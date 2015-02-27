e6ed33a224d45bbcd65af3ad5881c95d22a002e1
Jefry <jefrycruz88@gmail.com>
2015-02-26 22:13:48 -0500

    Created basic File model

---
+++ app/models/File.php
@@ -0,0 +1,15 @@
+<?php
+
+class File extends \Eloquent {
+	protected $fillable = [];
+
+
+    public function user(){
+        return $this->belongsTo('User');
+    }
+
+    public function project(){
+        return $this->belongsTo('Project');
+    }
+
+}
