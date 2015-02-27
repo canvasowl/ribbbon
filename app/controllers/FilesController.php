78d88a8f08bc23def9edb7eb125836ad17ed5438
Jefry <jefrycruz88@gmail.com>
2015-02-27 00:54:48 -0500

    Created the files controller

---
+++ app/controllers/FilesController.php
@@ -0,0 +1,23 @@
+<?php
+
+class FilesController extends \BaseController {
+
+
+    /**
+     * Upload the file and store
+     * the file path in the DB.
+     */
+	public function store()
+	{
+		//
+	}
+
+    /**
+     * Delete the given file
+     */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
