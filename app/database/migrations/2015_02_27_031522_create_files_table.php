3bb11300e5c372c2245879e3bafa233fc65f5891
Jefry <jefrycruz88@gmail.com>
2015-02-26 22:19:52 -0500

    Created files migration file

---
+++ app/database/migrations/2015_02_27_031522_create_files_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateFilesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('files', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->integer('user_id');
+            $table->integer('project_id');
+            $table->string('name');
+            $table->string('path');
+			$table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('files');
+	}
+
+}
