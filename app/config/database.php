37d3320a2f976236547c916b4211c950150f95bb
Jefry <jefrycruz88@gmail.com>
2015-02-26 21:41:16 -0500

    Reverted from env credentials

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => $_ENV['DB_HOST'],
-			'database'  => $_ENV['DB_NAME'],
-			'username'  => $_ENV['DB_USERNAME'],
-			'password'  => $_ENV['DB_PASSWORD'],
+			'host'      => 'root',
+			'database'  => 'root',
+			'username'  => 'forge',
+			'password'  => 'pdeBrYfcUgRPVZYX2m8e',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
