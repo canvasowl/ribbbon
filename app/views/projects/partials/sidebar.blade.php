bc64b88a789665afe121699d166aa1713c5eeabf
Jefry <jefrycruz88@gmail.com>
2015-02-27 00:58:27 -0500

    Added sidebar link for files

--- app/views/projects/partials/sidebar.blade.php
+++ app/views/projects/partials/sidebar.blade.php
@@ -2,6 +2,7 @@
 <ul class="nav nav-pills nav-stacked">
   <li><a href="/projects/{{ $project->id }}"><i class="fa fa-tasks"></i> Tasks</a></li>
   <li><a href="/projects/{{ $project->id }}/credentials"><i class="fa fa-key"></i> Credentials</a></li>
+  <li><a href="/projects/{{ $project->id }}/files"><i class="fa fa-folder"></i> Files</a></li>
   @if( isOwner($project->id) == true )
     <li><a href="/projects/{{ $project->id }}/manage"><i class="fa fa-cog"></i> Manage</a></li>
   @endif
@@ -20,4 +21,5 @@
   <div class="clearfix"></div>
 @endif
 {{--MEMBERS--}}
+
 
