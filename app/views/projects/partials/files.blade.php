820d7ae6a9959993f70b1ff1428161606b26f61f
Jefry <jefrycruz88@gmail.com>
2015-03-02 01:44:12 -0500

    Styled file button

--- app/views/projects/partials/files.blade.php
+++ app/views/projects/partials/files.blade.php
@@ -2,15 +2,18 @@
 <div><strong>Manage project files</strong></div><br>
 <div>
 	<div class="dynamic-form">
-		{{ Form::open(array('action' => 'FilesController@store', 'method' => 'post')) }}
+		{{ Form::open(array('action' => 'FilesController@store', 'method' => 'post', 'files' => true)) }}
 			<input class="form-control" type="text" name="name" placeholder="File Name">
      		<input type="hidden" name="project_id" value="{{ $project->id }}">
      		<br>
      		<div class="col-xs-4 no-padding-left">
-     			<input class="form-control other" type="text" name="port" placeholder="Port">
+                <div class="fileUpload btn btn-primary btn-wide">
+                    <span>Choose file</span>
+                    <input type="file" class="upload" />
+                </div>
      		</div>
      		<div class="col-xs-8 no-padding-right">
-         		<button type="submit" class="btn btn-default">
+         		<button type="submit" class="btn btn-default btn-wide">
 	                 <i class="fa fa-cloud-upload fa-lg"></i> Upload File
 	            </button>
      		</div>
