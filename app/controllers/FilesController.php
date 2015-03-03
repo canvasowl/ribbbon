5cf22498264b0d875490f21808496a97eb436186
Jefry <jefrycruz88@gmail.com>
2015-03-03 00:35:01 -0500

    Created upload and validation logic

--- app/controllers/FilesController.php
+++ app/controllers/FilesController.php
@@ -8,14 +8,31 @@ class FilesController extends \BaseController {
      */
 	public function store()
 	{
+        $rules = [ 'name' => 'required', 'file' => 'required|max:20000'];
+        $validator = Validator::make(Input::all(), $rules);
+
+        if( $validator->fails() ){
+            return Redirect::back()->withErrors($validator)->withInput();
+        }
+
         $directory = "uploads/files/";
 
         // Before anything let's make sure a file was uploaded
         if ( Input::hasFile('file') && Request::file('file')->isValid() )
         {
-            $file = Input::file('file');
-            $filename = Auth::id() .'_'. $file->getClientOriginalName();
-            $file->move($directory, $filename);
+
+            $current_file = Input::file('file');
+            $filename = Auth::id() .'_'. $current_file->getClientOriginalName();
+            $current_file->move($directory, $filename);
+
+            $file = new Upload;
+            $file->user_id = Auth::id();
+            $file->project_id = Input::get('project_id');
+            $file->name = Input::get('name');
+            $file->path = $directory.$filename;
+            $file->save();
+
+            return Redirect::back();
         }
 	}
 
