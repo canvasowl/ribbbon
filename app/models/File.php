177f2905daf48cc7f56470116c1787658df260a9
Jefry <jefrycruz88@gmail.com>
2015-03-03 00:05:12 -0500

    Created validation rules for file store form

--- app/models/File.php
+++ app/models/File.php
@@ -3,6 +3,12 @@
 class File extends \Eloquent {
 	protected $fillable = [];
 
+    // validation rules for files store form
+    public static $rules = [
+        'name' => 'required',
+        'file' => 'required|max:20000|',
+    ];
+
 
     public function user(){
         return $this->belongsTo('User');
