224dcd8b127970f848b21f5e11c7e86f34331eb2
Jefry <jefrycruz88@gmail.com>
2015-03-03 00:27:28 -0500

    Removed validation rules (was buggy)

--- app/models/File.php
+++ app/models/File.php
@@ -3,13 +3,6 @@
 class File extends \Eloquent {
 	protected $fillable = [];
 
-    // validation rules for files store form
-    public static $rules = [
-        'name' => 'required',
-        'file' => 'required|max:20000|',
-    ];
-
-
     public function user(){
         return $this->belongsTo('User');
     }
