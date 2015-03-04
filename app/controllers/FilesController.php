2902ce55f0107bda4355071875ce19a196ce01ea
Jefry <jefrycruz88@gmail.com>
2015-03-03 19:30:48 -0500

    Store method now works

--- app/controllers/FilesController.php
+++ app/controllers/FilesController.php
@@ -8,6 +8,17 @@ class FilesController extends \BaseController {
      */
 	public function store()
 	{
+        // Rules
+        $rules	= array('name' => 'required', 'file' => 'required|max:20000');
+        $messages = array('max' => 'Please make sure the file size is not larger then 20MB');
+
+        // Create validation
+        $validator = Validator::make( Input::all(), $rules, $messages);
+
+        if ( $validator->fails() ) {
+            return Redirect::back()->withErrors($validator)->withInput();
+        }
+
         $directory = "uploads/files/";
 
         // Before anything let's make sure a file was uploaded
@@ -17,6 +28,15 @@ class FilesController extends \BaseController {
             $filename = Auth::id() .'_'. $file->getClientOriginalName();
             $file->move($directory, $filename);
         }
+
+        $upload = new Upload;
+        $upload->user_id = Auth::id();
+        $upload->project_id = Input::get('project_id');
+        $upload->name = Input::get('name');
+        $upload->path = $directory.$filename;
+        $upload->save();
+
+        return Redirect::back();
 	}
 
     /**
