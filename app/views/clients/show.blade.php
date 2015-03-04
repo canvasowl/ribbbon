44dc789a20b6234433337c99731bef13ce25361c
Jefry <jefrycruz88@gmail.com>
2015-03-04 04:20:31 -0500

    Style changes

--- app/views/clients/show.blade.php
+++ app/views/clients/show.blade.php
@@ -4,17 +4,13 @@
 @section('content')
 
 <div class="row main-row">
-
     <div class="col-xs-12">
-         <div>
-            <h2 class="pull-left no-margin-top">{{ $client->name }}</h2>                                   
-            <ul class="list-inline pull-right">
-                <li><a title="Go back" class="btn " href="/clients"><i class="fa fa-arrow-circle-o-left fa-lg"></i></a></li>
-                <li><a title="Edit client" class="btn " href="/clients/{{ $client->id }}/edit"><i class="fa fa-pencil-square-o fa-lg"></i></a></li>
-            </ul>
-            <div class="clearfix"></div>   
-            <hr>                 
-        </div>
+        <h2 class="pull-left">{{ $client->name }}</h2>
+        <ul class="list-inline pull-right">
+            <li><a title="Go back" class="btn " href="/clients"><i class="fa fa-arrow-circle-o-left fa-lg"></i></a></li>
+            <li><a title="Edit client" class="btn " href="/clients/{{ $client->id }}/edit"><i class="fa fa-pencil-square-o fa-lg"></i></a></li>
+        </ul>
+        <div class="clearfix"></div>
     </div>
 
 	<div class="col-xs-12">
