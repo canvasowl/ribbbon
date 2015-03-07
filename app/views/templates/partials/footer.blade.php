498c358bcc397533c83a51c698fba7c153d3d357
Jefry <jefrycruz88@gmail.com>
2015-03-07 03:04:28 -0500

    Removed css a cleaned footer file

--- app/views/templates/partials/footer.blade.php
+++ app/views/templates/partials/footer.blade.php
@@ -1,101 +1,75 @@
+<div class="hug hug-footerIn">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <center>
+                    <ul class="list-inline">
+                        <li><a href="/about">About Ribbbon</a></li>
+                        <li><a href="/faq">FAQ</a></li>
+                    </ul>
+                    <ul class="list-inline">
+                        <li>Developed with<a target="_blank" href="http://laravel.com/"> Laravel</a> by <a target="_blank" href="http://www.punyweblab.com/"> Jefry Cruz.</a></li>
+                    </ul>
+                    <p>COPYRIGHT {{ date('Y') }} &copy; JEFRY CRUZ</p>
+                </center>
+            </div>
+        </div>
+    </div>
+</div>
 
-@if ( !Auth::check() && Request::is('/')  || Request::is('about') || Request::is('faq')  )
-	<div class="hug hug-footerOut">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-12 col-md-6">
-					<ul class="list-inline list-style-none">
-						<li><a href="/about">About <span class="site-name">Ribbbon</span></a></li>
-						<li><a href="/faq">FAQ</a></li>
-					</ul>	
-					<ul class="list-inline list-style-none">						
-						<li>Developed with<a target="_blank" href="http://laravel.com/"> Laravel</a> by <a target="_blank" href="http://www.punyweblab.com/"> Jefry Cruz.</a></li>						
-					</ul>					
-				</div>
-				<div class="col-xs-12 col-md-6">
-					<ul class="list-inline list-style-none pull-right">					
-						<li>Copyright {{ date('Y') }} &copy; Jefry Cruz</li>
-					</ul>				
-				</div>
-			</div>
-		</div>
-	</div>
-@else
-	<div class="hug hug-footerIn">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-12">
-					<center>
-						<ul class="list-inline">
-							<li><a href="/about">About Ribbbon</a></li>
-							<li><a href="/faq">FAQ</a></li>							
-						</ul>
-						<ul class="list-inline">
-							<li>Developed with<a target="_blank" href="http://laravel.com/"> Laravel</a> by <a target="_blank" href="http://www.punyweblab.com/"> Jefry Cruz.</a></li>
-						</ul>
-						<p>COPYRIGHT {{ date('Y') }} &copy; JEFRY CRUZ</p>
-					</center>				
-				</div>
-			</div>
-		</div>
-	</div>
 
-<!-- Uservoice Feedback form -->
-<script>
-// Include the UserVoice JavaScript SDK (only needed once on a page)
-UserVoice=window.UserVoice||[];(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/HuToQLQVTIvvZeFkrVBNfQ.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
+@if ( Auth::check() && !Request::is('/')  || !Request::is('about') || !Request::is('faq')  )
+    <!-- Uservoice Feedback form -->
+    <script>
+    // Include the UserVoice JavaScript SDK (only needed once on a page)
+    UserVoice=window.UserVoice||[];(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/HuToQLQVTIvvZeFkrVBNfQ.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
 
-//
-// UserVoice Javascript SDK developer documentation:
-// https://www.uservoice.com/o/javascript-sdk
-//
+    //
+    // UserVoice Javascript SDK developer documentation:
+    // https://www.uservoice.com/o/javascript-sdk
+    //
 
-// Set colors
-UserVoice.push(['set', {
-  accent_color: '#808283',
-  trigger_color: 'white',
-  trigger_background_color: 'rgba(46, 49, 51, 0.6)'
-}]);
+    // Set colors
+    UserVoice.push(['set', {
+      accent_color: '#808283',
+      trigger_color: 'white',
+      trigger_background_color: 'rgba(46, 49, 51, 0.6)'
+    }]);
 
-// Identify the user and pass traits
-// To enable, replace sample data with actual user traits and uncomment the line
-UserVoice.push(['identify', {
-  //email:      'john.doe@example.com', // User’s email address
-  //name:       'John Doe', // User’s real name
-  //created_at: 1364406966, // Unix timestamp for the date the user signed up
-  //id:         123, // Optional: Unique id of the user (if set, this should not change)
-  //type:       'Owner', // Optional: segment your users by type
-  //account: {
-  //  id:           123, // Optional: associate multiple users with a single account
-  //  name:         'Acme, Co.', // Account name
-  //  created_at:   1364406966, // Unix timestamp for the date the account was created
-  //  monthly_rate: 9.99, // Decimal; monthly rate of the account
-  //  ltv:          1495.00, // Decimal; lifetime value of the account
-  //  plan:         'Enhanced' // Plan name for the account
-  //}
-}]);
+    // Identify the user and pass traits
+    // To enable, replace sample data with actual user traits and uncomment the line
+    UserVoice.push(['identify', {
+      //email:      'john.doe@example.com', // User’s email address
+      //name:       'John Doe', // User’s real name
+      //created_at: 1364406966, // Unix timestamp for the date the user signed up
+      //id:         123, // Optional: Unique id of the user (if set, this should not change)
+      //type:       'Owner', // Optional: segment your users by type
+      //account: {
+      //  id:           123, // Optional: associate multiple users with a single account
+      //  name:         'Acme, Co.', // Account name
+      //  created_at:   1364406966, // Unix timestamp for the date the account was created
+      //  monthly_rate: 9.99, // Decimal; monthly rate of the account
+      //  ltv:          1495.00, // Decimal; lifetime value of the account
+      //  plan:         'Enhanced' // Plan name for the account
+      //}
+    }]);
 
-// Add default trigger to the bottom-right corner of the window:
-UserVoice.push(['addTrigger', { mode: 'contact', trigger_position: 'bottom-right' }]);
+    // Add default trigger to the bottom-right corner of the window:
+    UserVoice.push(['addTrigger', { mode: 'contact', trigger_position: 'bottom-right' }]);
 
-// Or, use your own custom trigger:
-//UserVoice.push(['addTrigger', '#id', { mode: 'contact' }]);
+    // Or, use your own custom trigger:
+    //UserVoice.push(['addTrigger', '#id', { mode: 'contact' }]);
 
-// Autoprompt for Satisfaction and SmartVote (only displayed under certain conditions)
-UserVoice.push(['autoprompt', {}]);
-</script>	
+    // Autoprompt for Satisfaction and SmartVote (only displayed under certain conditions)
+    UserVoice.push(['autoprompt', {}]);
+    </script>
 @endif
-
 
 <!-- js -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="{{ asset('assets/js/Chart.js') }}"></script>
 <script type="text/javascript" src="{{ asset('assets/js/main.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/js/hud.js') }}"></script>		
-
-
-
-
+<script type="text/javascript" src="{{ asset('assets/js/hud.js') }}"></script>
 </body>
 </html>
--- app/views/templates/partials/head.blade.php
+++ app/views/templates/partials/head.blade.php
@@ -17,13 +17,13 @@
 
 	@if ( Request::is('/') && !Auth::check() )
 		<style type="text/css">
-			html { 
-			  background: url(/assets/img/home_bg.png) no-repeat center center fixed; 
-			  -webkit-background-size: cover;
-			  -moz-background-size: cover;
-			  -o-background-size: cover;
-			  background-size: cover;
-			}
+                /*html { */
+                  /*background: url(/assets/img/home_bg.png) no-repeat center center fixed; */
+                  /*-webkit-background-size: cover;*/
+                  /*-moz-background-size: cover;*/
+                  /*-o-background-size: cover;*/
+                  /*background-size: cover;*/
+                /*}*/
 			
 			.hug-footerOut{position: absolute; z-index: 100; bottom: 0; left: 0; }
 			
@@ -39,21 +39,21 @@
 	@endif
 
 	@if ( !Auth::check() && Request::is('/') || Request::is('beta') || Request::is('register') || Request::is('signin') || Request::is('about') || Request::is('faq') )
-		<style type="text/css">
-			.row{width: 900px; max-width: 100%; margin: 0 auto;}		
-		</style>
+		{{--<style type="text/css">--}}
+			{{--.row{width: 900px; max-width: 100%; margin: 0 auto;}		--}}
+		{{--</style>--}}
 	@endif	
 
 	@if ( Request::is('about') || Request::is('faq')  )
 		<style type="text/css">
-			.container{padding-left: 0; padding-right: 0;}		
+			/*.container{padding-left: 0; padding-right: 0;}		*/
 		</style>
 	@endif	
 
 	@if ( Request::is('beta') || Request::is('register') || Request::is('signin') )
-		<style type="text/css">
-			.hug-homeBody .container{height: auto;}
-		</style>		
+		{{--<style type="text/css">--}}
+			{{--.hug-homeBody .container{height: auto;}--}}
+		{{--</style>		--}}
 	@endif
 
 	@if ( Request::is('about') )
