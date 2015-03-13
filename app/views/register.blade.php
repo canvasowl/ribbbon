a0675f85efdf95b27cb4c6b3d6f01c33c0c74b46
Jefry <jefrycruz88@gmail.com>
2015-03-12 21:05:24 -0400

    Updated register and login form

--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -3,25 +3,27 @@
 
 @section('content')
 
-	
-  <div class="tab-pane active home-form" id="login">
-  	<center><h1>Register For Free!</h1></center>
-	<div>										
-		{{ Form::open(array('action' => 'UsersController@register')) }}
-			<div class="form-group">				
-				{{ Form::text('fullName', null, array('class' => 'form-control', "placeholder" => "full name", "autofocus" => "true" )) }}	
-			</div>				
-			<div class="form-group">
-				{{ Form::text('email', null, array('class' => 'form-control', "placeholder" => "email" )) }}	
-			</div>
-			<div class="form-group">
-				{{ Form::password('password', array('class' => 'form-control', "placeholder" => "password" )) }}	
-			</div>
-			<div class="form-group">
-				{{ Form::submit('Signup', array('class' => 'btn btn-primary btn-wide' )); }}
-			</div>	    			
-		{{ Form::close() }}
-	</div>		  	
+
+  <div class="special-form">
+      <div class="panel panel-default">
+        <div class="panel-heading">Register For Free!</div>
+            <div class="panel-body">
+                {{ Form::open(array('action' => 'UsersController@register')) }}
+                    <div class="form-group">
+                        {{ Form::text('fullName', null, array('class' => 'form-control', "placeholder" => "full name", "autofocus" => "true" )) }}
+                    </div>
+                    <div class="form-group">
+                        {{ Form::text('email', null, array('class' => 'form-control', "placeholder" => "email" )) }}
+                    </div>
+                    <div class="form-group">
+                        {{ Form::password('password', array('class' => 'form-control', "placeholder" => "password" )) }}
+                    </div>
+                    <div class="form-group">
+                        {{ Form::submit('Signup', array('class' => 'btn btn-primary btn-wide' )); }}
+                    </div>
+                {{ Form::close() }}
+            </div>
+      </div>
   </div>
 
 
