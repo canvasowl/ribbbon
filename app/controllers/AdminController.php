5a92095e9c9c22754acb60e64e6ac82035d12db9
Jefry <jefrycruz88@gmail.com>
2015-05-06 00:01:07 -0400

    invite method now returns a simple message string

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -8,20 +8,22 @@ class AdminController extends \BaseController {
 	 */
 	public function invite(){
 
+        return "Do nothing. no invites sent";
+
 		// Get the emails to invite
-		$users 		=	Beta::whereStatus(0)->lists('email');
-		// At this email always for testing
-		$users[]	=	"jefrycruz88@gmail.com"; 
+//		$users 		=	Beta::whereStatus(0)->lists('email');
+//		// At this email always for testing
+//		$users[]	=	"jefrycruz88@gmail.com";
 
 		// Set the all statuses to 0
-		Beta::whereStatus(0)->update(array('status' => 1));
+//		Beta::whereStatus(0)->update(array('status' => 1));
 
 		// Send the emails to all the users that have not been invited yet
-		for ($i=0; $i < count($users) ; $i++) { 			
-			sendBetaInviteEmail( $users[$i]);			
-			echo $users[$i] . "<br>";
-		}
-
-		return "All the emails above have been sent a beta invite.";
+//		for ($i=0; $i < count($users) ; $i++) {
+//			sendBetaInviteEmail( $users[$i]);
+//			echo $users[$i] . "<br>";
+//		}
+//
+//		return "All the emails above have been sent a beta invite.";
 	}
 }
