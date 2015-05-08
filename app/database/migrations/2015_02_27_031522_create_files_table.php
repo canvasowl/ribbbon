e9bd82df89535ff37e2357b1e370e2b6de91b8df
Jefry <jefrycruz88@gmail.com>
2015-05-07 20:37:36 -0400

    Refactor migration class

--- app/database/migrations/2015_02_27_031522_create_files_table.php
+++ app/database/migrations/2015_02_27_031522_create_files_table.php
@@ -5,36 +5,36 @@ use Illuminate\Database\Schema\Blueprint;
 
 class CreateFilesTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('uploads', function(Blueprint $table)
-		{
-			$table->increments('id');
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('uploads', function(Blueprint $table)
+        {
+            $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->integer('project_id')->unsigned();
             $table->string('name');
             $table->string('path');
-			$table->timestamps();
+            $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
-		});
-	}
+        });
+    }
 
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('uploads');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('uploads');
+    }
 
 }
