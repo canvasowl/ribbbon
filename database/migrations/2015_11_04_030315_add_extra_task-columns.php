849254f06ea214706e760b5121a1dde93d1be2f4
Jefry <jefrycruz88@gmail.com>
2015-11-03 22:20:48 -0500

    Created new tasks schema migration for description and due_date

---
+++ database/migrations/2015_11_04_030315_add_extra_task-columns.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddExtraTaskColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('description');
+            $table->string('due_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn(['description', 'due_date']);
+        });
+    }
+}
