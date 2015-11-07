a1a78b8ba6a54df1e1d2fa517219d3c30055b6fa
Jefry <jefrycruz88@gmail.com>
2015-11-07 12:22:03 -0500

    Removed created_at and updated_at from return fields

--- app/Client.php
+++ app/Client.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model {
 
 	protected $table = 'clients';
+	protected $hidden = ['created_at','updated_at'];
 	protected $fillable = [
 		'user_id',
 		'name',
