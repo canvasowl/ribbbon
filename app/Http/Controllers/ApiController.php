51fa4c1e11589a1f841267242fd0320441249128
Jefry <jefrycruz88@gmail.com>
2015-11-11 22:20:44 -0500

    Refactor - Updated Api calls to use mutator methods

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -16,12 +16,46 @@ use App\Credential;
 
 class ApiController extends BaseController {
 
-	/**
-	 * Get the id of the Auth user
-	 * @param  [int] $key the api key
-	 * @return [int]      An id
-	 */
-	public function authId($key){
+	private $statusCode = 200;
+
+    /**
+     * @return int
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * @param $statusCode
+     * @return $this
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+
+        return $this;
+    }
+
+    /**
+     * @param $message
+     * @param null $data
+     * @return mixed
+     */
+    public function makeResponse($message, $data = null)
+    {
+        return Response::json([
+            'status_code' => $this->getStatusCode(),
+            'message' => $message,
+            'data' => $data
+        ], $this->getStatusCode());
+    }
+
+    /**
+     * @param $key
+     * @return string or Int
+     */
+    public function authId($key){
 		
 		// Validate the api key
 		if ($key != 0000000000) {
@@ -33,129 +67,83 @@ class ApiController extends BaseController {
 
 	// CLIENT - Get all clients for the logged in user
 	public function getAllUserClients(){
-		$clients = Client::where('user_id',Auth::id())->get();
+		$clients = Client::where('user_id',4)->get();
 		
-		if (count($clients) === 0) {			
-			return Response::json([
-				'status' => 'error',
-				'message' => 'No clients found'
-			],404);
-		}
-
-		return Response::json([
-				'status' => 'ok',
-				'message' => 'Clients retrived successfully',
-				'data' => $clients->toArray()
-			],200);
+		if (count($clients) === 0) {
+            return $this->setStatusCode(400)->makeResponse('Could not find clients');
+        }
+        return $this->setStatusCode(200)->makeResponse('Clients retrieved successfully',$clients->toArray());
 	}
 
 	// CLIENT - create a new client
 	public function storeClient(){		
 		if (!Input::all()) {
-			return Response::json([
-				'status' => 'error',
-				'message' => 'No information provided to create client'
-			],406);			
+            return $this->setStatusCode(406)->makeResponse('No information provided to create client');
 		}
 
 		Input::merge(array('user_id' => Auth::id()));
 		Client::create(Input::all());			
 		$id = \DB::getPdo()->lastInsertId();
 
-	    return Response::json([
-			'status' => 'ok',
-			'message' => 'Client created successfully',
-			'data' => Client::find($id)
-		],200);			
+        return $this->setStatusCode(200)->makeResponse('Client created successfully', Client::find($id));
 	}
 
 	// CLIENT - update a specific client
 	public function updateClient($id){
 		if (count(Input::all()) <= 1) {
-			return Response::json([
-				'status' => 'error',
-				'message' => 'No information provided to update client'
-			],406);			
+            return $this->setStatusCode(406)->makeResponse('No information provided to update client');
 		}
 
 		if (!Client::find($id)) {
-			return Response::json([
-				'status' => 'error',
-				'message' => 'Client not found'
-			],404);
+            return $this->setStatusCode(404)->makeResponse('Client not found');
 		}
 
 		$input = Input::all();
 		unset($input['_method']);
 
 		Client::find($id)->update($input);
-		
-		return Response::json([
-			'status' => 'ok',
-			'message' => 'The client has been updated'
-		],200);
+
+        return $this->setStatusCode(200)->makeResponse('The client has been updated');
 	}
 
 	// CLIENT - get a specific client 
 	public function getClient($id){
 		if (!Client::find($id)) {
-			return Response::json([
-				'status' => 'error',
-				'message' => 'The client was not found'
-			],404);			
+            return $this->setStatusCode(404)->makeResponse('The client was not found');
 		}
-	    return Response::json([
-			'status' => 'ok',
-			'message' => 'Client was successfully found',
-			'data' => Client::find($id)
-		],200);		
+
+        return $this->setStatusCode(200)->makeResponse('Client was successfully found', Client::find($id));
 	}
 
 	// PROJECT - store a new project
 	public function storeProject(){
 		if (!Input::all()) {
-			return Response::json([
-				'status' => 'error',
-				'message' => 'No information provided to create project'
-			],406);			
+            return $this->setStatusCode(406)->makeResponse('No information provided to create project');
 		}
 
 		Input::merge(array('user_id' => 1));
 		Project::create(Input::all());			
 		$id = \DB::getPdo()->lastInsertId();
 
-	    return Response::json([
-			'status' => 'ok',
-			'message' => 'Project created successfully',
-			'data' => Project::find($id)
-		],200);			
+        return $this->setStatusCode(200)->makeResponse('Project created successfully', Project::find($id));
 	}
 
-	// PROJEC - update project
+	// PROJECT - update project
 	public function updateProject($id){
 		if (count(Input::all()) <= 1) {
-			return Response::json([
-				'status' => 'error',
-				'message' => 'No information provided to update project'
-			],406);			
+            return $this->setStatusCode(406)->makeResponse('No information provided to update project');
 		}
 
 		if (!Project::find($id)) {
-			return Response::json([
-				'status' => 'error',
-				'message' => 'Project not found'
-			],404);
+            return $this->setStatusCode(404)->makeResponse('Project not found');
 		}
 
 		$input = Input::all();
 		unset($input['_method']);
 
 		Project::find($id)->update($input);
-		
-		return Response::json([
-			'status' => 'ok',
-			'message' => 'The project has been updated'
-		],200);
-	}	
-	
+
+        return $this->setStatusCode(200)->makeResponse('The project has been updated');
+	}
+
 }
