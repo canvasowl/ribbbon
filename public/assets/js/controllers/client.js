f2a141acaced0122c9c242b7b17c90b876d35b49
Jefry <jefrycruz88@gmail.com>
2015-12-19 20:47:13 -0500

    Created Vue,js method for creating client

--- public/assets/js/controllers/client.js
+++ public/assets/js/controllers/client.js
@@ -1,6 +1,38 @@
 var client = new Vue({
   el: '#client',
   data: {
-    message: 'Hello Vue.js!'
-  }
+    client: { name : null, phone_number : null, point_of_contact : null, email : null},
+    lastRequest: false,
+
+  },
+
+  methods: {
+  	create: function(client){
+		event.preventDefault();
+
+		$.ajax({
+		  type: 'POST',
+		  url: "/api/clients",
+		  data: client,
+		  error: function(e) {
+		    var response = jQuery.parseJSON(e.responseText);
+		  	$('.new-client .status-msg').text("")
+		  								.addClass("error-msg")
+		  								.text(response.message);			    
+		  	return false;
+		  },
+		  success: function(data){		  	
+		  	$('.new-client .status-msg').text("")
+		  								.addClass("success-msg")
+		  								.text(data.message);		    		  	
+		  	
+		  	client.name = null;
+		  	client.phone_number = null;
+		  	client.point_of_contact = null;
+		  	client.email = null;
+		  }
+		}); 
+  	}
+  },
+
 })
