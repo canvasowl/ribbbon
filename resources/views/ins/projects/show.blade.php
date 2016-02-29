38005fc338340a6e91edb674e9cc23478b0f5976
Jefry <jefrycruz88@gmail.com>
2016-02-28 21:47:37 -0500

    UI and style changes for projects page

--- resources/views/ins/projects/show.blade.php
+++ resources/views/ins/projects/show.blade.php
@@ -5,10 +5,11 @@
 <div id="project">
     <div class="row" xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-on="http://www.w3.org/1999/xhtml">
         <div class="col-xs-12 page-title-section">
-            <h1 class="pull-left">Project Name</h1>
+            <h1 class="pull-left">@{{ project.name }}</h1>
+
             <div class="clearfix"></div>
             <p class="dim">Progress</p>
-            <div class="col-xs-10 no-padding-left">
+            <div class="col-xs-11 no-padding-left">
                 <div class="progress">
                     <div class="progress-bar" role="progressbar" aria-valuenow="70"
                          aria-valuemin="0" aria-valuemax="100" style="width:70%">
@@ -16,8 +17,9 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xs-2 no-margin-right">
-                <span class="weight">w.10</span>
+            <div class="col-xs-1 no-margin-right">
+                <div class="pull-right"><span class="weight">w.10</span></div>
+                <div class="clearfix"></div>
             </div>
         </div>
     </div>
@@ -26,7 +28,7 @@
         <div class="col-xs-12">
             <div class="main-section">
                 <div class="pull-right">
-                    <button style="position: relative; z-index: 10" class="btn btn-primary"><span class="ion-plus-circled"></span> New Task</button>
+                    <button v-on:click="showTaskCreateForm()" style="position: relative; z-index: 10" class="btn btn-primary"><span class="ion-plus-circled"></span> New Task</button>
                 </div>
                 <div class="mega-menu mega-menu-tab">
                     <div class="links">
@@ -42,9 +44,9 @@
                                 <div class="col-xs-12 col-md-4">
                                     <ul class="task-list">
                                         <h5 class="title">In Progress (10)</h5>
-                                        <li>
+                                        <li class="priority-a">
                                             <div>
-                                                <div class="pull-left color-badge">w.30</div>
+                                                <div class="pull-left">w.30</div>
                                                 <div class="show-on-hover pull-right">
                                                     <span class="ion-close-round"></span>
                                                     <span class="ion-edit"></span>
@@ -54,14 +56,58 @@
                                             <div>
                                                 <h5>Task Name</h5>
                                             </div>
-                                            <footer>
-                                                <select class="form-control show-on-hover">
-                                                    <option value="backlog">backlog</option>
-                                                    <option value="progress">Progress</option>
-                                                    <option value="testing">Testing</option>
-                                                    <option value="Completed">Completed</option>
-                                                </select>
-                                            </footer>
+                                        </li>
+                                        <li class="priority-b">
+                                            <div>
+                                                <div class="pull-left">w.30</div>
+                                                <div class="show-on-hover pull-right">
+                                                    <span class="ion-close-round"></span>
+                                                    <span class="ion-edit"></span>
+                                                </div>
+                                                <div class="clearfix"></div>
+                                            </div>
+                                            <div>
+                                                <h5>Task Name</h5>
+                                            </div>
+                                        </li>
+                                        <li class="priority-c">
+                                            <div>
+                                                <div class="pull-left">w.30</div>
+                                                <div class="show-on-hover pull-right">
+                                                    <span class="ion-close-round"></span>
+                                                    <span class="ion-edit"></span>
+                                                </div>
+                                                <div class="clearfix"></div>
+                                            </div>
+                                            <div>
+                                                <h5>Task Name</h5>
+                                            </div>
+                                        </li>
+                                        <li class="priority-d">
+                                            <div>
+                                                <div class="pull-left">w.30</div>
+                                                <div class="show-on-hover pull-right">
+                                                    <span class="ion-close-round"></span>
+                                                    <span class="ion-edit"></span>
+                                                </div>
+                                                <div class="clearfix"></div>
+                                            </div>
+                                            <div>
+                                                <h5>Task Name</h5>
+                                            </div>
+                                        </li>
+                                        <li class="priority-e">
+                                            <div>
+                                                <div class="pull-left">w.30</div>
+                                                <div class="show-on-hover pull-right">
+                                                    <span class="ion-close-round"></span>
+                                                    <span class="ion-edit"></span>
+                                                </div>
+                                                <div class="clearfix"></div>
+                                            </div>
+                                            <div>
+                                                <h5>Task Name</h5>
+                                            </div>
                                         </li>
                                     </ul>
                                 </div>
@@ -87,7 +133,7 @@
         </div>
     </div>
 
-    <div class="popup-form new-task">
+    <div style="z-index: 20" class="popup-form new-task">
         <header>
             <p class="pull-left">New Task</p>
             <div class="actions pull-right">
@@ -99,18 +145,46 @@
         <section>
             <form>
                 <span class="status-msg"></span>
-                <input v-model="newTask.name" placeholder="Name" type="text" class="form-control first">
-                <input v-model="newTask.weight" placeholder="Weight" type="number" class="form-control first">
-                <select v-model="newTask.state" class="form-control">
-                    <option selected>backlog</option>
-                    <option>progress</option>
-                    <option>testing</option>
-                    <option>complete</option>
-                </select>
+                <div class="col-xs-12 no-side-padding">
+                    <input v-model="newTask.name" placeholder="Name" type="text" class="form-control first">
+                </div>
+                <div class="col-xs-4 no-side-padding">
+                    <select v-model="newTask.weight" class="form-control">
+                        <option selected>Weight</option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        <option>6</option>
+                        <option>7</option>
+                        <option>8</option>
+                        <option>9</option>
+                        <option>10</option>
+                    </select>
+                </div>
+                <div class="col-xs-4">
+                    <select v-model="newTask.priority" class="form-control">
+                        <option selected>Priority</option>
+                        <option>Low Priority</option>
+                        <option>Normal Priority</option>
+                        <option>Medium Priority</option>
+                        <option>High Priority</option>
+                        <option>Highest Priority</option>
+                    </select>
+                </div>
+                <div class="col-xs-4 no-side-padding">
+                    <select v-model="newTask.state" class="form-control">
+                        <option selected>backlog</option>
+                        <option>progress</option>
+                        <option>testing</option>
+                        <option>complete</option>
+                    </select>
+                </div>
             </form>
         </section>
         <footer>
-            <a v-on:click="createProject(true)" href="" class="btn btn-primary pull-right">Save</a>
+            <a v-on:click="createProject(client_id)" href="" class="btn btn-primary pull-right">Save</a>
             <div class="clearfix"></div>
         </footer>
     </div>
