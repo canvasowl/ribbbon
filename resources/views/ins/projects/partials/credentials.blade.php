06f989380397eceee308ef874c0aad7ca7eb8e16
Jefry <jefrycruz88@gmail.com>
2016-04-09 11:50:40 -0400

    File cleanup

--- resources/views/ins/projects/partials/credentials.blade.php
+++ resources/views/ins/projects/partials/credentials.blade.php
@@ -38,7 +38,7 @@
     <div v-on:click="editCredential(credential)" v-for="credential in project.credentials" class="col-xs-12 col-md-4 credential-@{{ credential.id }}">
         <div class="credential">
             <div>
-                <label class="pull-left">w.@{{ credential.name }}</label>
+                <label class="pull-left">@{{ credential.name }}</label>
                 <div class="show-on-hover pull-right">
                     <span v-on:click="deleteCredential(credential.id)" class="ion-close-round"></span>
                 </div>
@@ -51,79 +51,3 @@
         </div>
     </div>
 </div>
-
-
-{{--@if( count($credentials) > 0)--}}
-        {{--<!-- server panel -->--}}
-{{--<div>--}}
-    {{--<div><strong>FTP & SSH Accounts</strong></div>--}}
-    {{--<div class="ftp-panel-body">--}}
-        {{--<div class="row">--}}
-            {{--@foreach ($credentials as $credential)--}}
-                {{--@if ($credential->type == true)--}}
-                    {{--<div class="col-xs-12 col-md-6 credential-wrap" id="credential-wrap-{{ $credential->id }}"><br>--}}
-                        {{--<section class="info">--}}
-                            {{--<h4>{{ $credential->name }}</h4	>--}}
-                            {{--<p><strong>Hostname:</strong> {{ $credential->hostname }}</p>--}}
-                            {{--<p><strong>Username:</strong> {{ $credential->username }}</p>--}}
-                            {{--<p><strong>Password:</strong> {{ $credential->password }}</p>--}}
-                            {{--<p><strong>Port:</strong> {{ $credential->port }}</p>--}}
-
-                            {{--@if($owner_id == Auth::id())--}}
-                                {{--<div class="actions">--}}
-                                    {{--<ul class="inline-list list-style-none">--}}
-                                        {{--<li>--}}
-                                            {{--{!! Form::open(array('action' => 'CredentialsController@destroy', 'method' => 'delete')) !!}--}}
-                                            {{--<input type="hidden" name="id" value="{!! $credential->id !!}">--}}
-                                            {{--<button title="delete" id="{{ $credential->id }}" type="submit" class="btn btn-default btn-delete"><i class="fa fa-trash"></i></button>--}}
-                                            {{--{!! Form::close() !!}--}}
-                                        {{--</li>--}}
-                                        {{--<?php /*<li>--}}
-                                            {{--<button title="edit" class="btn btn-default"><a href=""><i class="fa fa-pencil"></i></a></button>--}}
-                                        {{--</li>*/ ?>--}}
-                                        {{--<div class="clearfix"></div>--}}
-                                    {{--</ul>--}}
-                                {{--</div>--}}
-                            {{--@endif--}}
-                        {{--</section>--}}
-                    {{--</div>--}}
-                {{--@endif--}}
-            {{--@endforeach--}}
-        {{--</div>--}}
-    {{--</div>--}}
-{{--</div><br>--}}
-
-{{--<!-- other panel -->--}}
-{{--<div>--}}
-    {{--<div><strong>Other Credentials</strong></div>--}}
-    {{--<div class="other-panel-body">--}}
-        {{--<div class="row">--}}
-            {{--@foreach ($credentials as $credential)--}}
-                {{--@if ($credential->type == false)--}}
-                    {{--<div class="col-xs-12 col-md-6 credential-wrap" id="credential-wrap-{{ $credential->id }}"><br>--}}
-                        {{--<section class="info">--}}
-                            {{--<h4>{{ $credential->name }}</h4	>--}}
-                            {{--<p><strong>Username:</strong> {{ $credential->username }}</p>--}}
-                            {{--<p><strong>Password:</strong> {{ $credential->password }}</p>--}}
-
-                            {{--@if($owner_id == Auth::id())--}}
-                                {{--<div class="actions">--}}
-                                    {{--<ul class="inline-list list-style-none">--}}
-                                        {{--<li>--}}
-                                            {{--{!! Form::open(array('action' => 'CredentialsController@destroy', 'method' => 'delete')) !!}--}}
-                                            {{--<input type="hidden" name="id" value="{{ $credential->id }}">--}}
-                                            {{--<button title="delete" id="{{ $credential->id }}" type="submit" class="btn btn-default btn-delete"><i class="fa fa-trash"></i></button>--}}
-                                            {{--{!! Form::close() !!}--}}
-                                        {{--</li>--}}
-                                        {{--<div class="clearfix"></div>--}}
-                                    {{--</ul>--}}
-                                {{--</div>--}}
-                            {{--@endif--}}
-                        {{--</section>--}}
-                    {{--</div>--}}
-                {{--@endif--}}
-            {{--@endforeach--}}
-        {{--</div>--}}
-    {{--</div>--}}
-{{--</div>--}}
-{{--@endif--}}
