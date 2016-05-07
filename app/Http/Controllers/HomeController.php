f765474e0d7ef2b96a7b974f6dd07597e757a750
Jefry <jefrycruz88@gmail.com>
2016-05-07 15:39:32 -0400

    Refactor HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,32 +15,14 @@ class HomeController extends BaseController {
 	// Depending if the user is signed in or not, return the home page 
 	public function index(){
 		if( Auth::check() ) {
-			$pTitle			=	"Hud";
-			$latestTasks	=	Task::where('user_id', Auth::id())->where('state','incomplete')->orderBy('created_at', 'desc')->take(5)->get();
-
-			$aWeekAgo		=	$start = date('Y-m-d 00:00:00',strtotime("-1 week"));
-            $latestCompletedTasks = Task::where('user_id', Auth::id())
-            							->where('state','complete')
-            							->where('updated_at','>',$aWeekAgo)
-            							->orderBy('updated_at', 'desc')->take(10)->get();
-
-			$latestProjects	=	Project::where('user_id', Auth::id())->orderBy('created_at', 'desc')->take(5)->get();
-			$user 		    =   User::find(Auth::id());
-			$inProjects     =   $user->inProjects()->orderBy('created_at', 'desc')->take(5)->get();
-
-
-			return View::make('ins/hud', compact('pTitle', 'latestProjects', 'latestTasks', 'latestCompletedTasks', 'inProjects'));
+			return View::make('ins/hud')->with('pTitle', "Hud");
 		}else{
 			return View::make('index')->with('pTitle', "A project management system for artisans");
 		}
 	}
 
-	/**
-	 * Run a general search.
-	 * @return  array of objects with the search results.
-	 */
+	// Search for, clients, projects, and tasks
 	public function search(){
-
 		$q = Input::get("q");
 
         // redirect user back if nothing was typed
