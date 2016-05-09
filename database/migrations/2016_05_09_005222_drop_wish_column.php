43602a45eb925debc8612ef23880b6ce2ff7255c
Jefry <jefrycruz88@gmail.com>
2016-05-08 21:19:18 -0400

    Added new migration to drop wish column

---
+++ database/migrations/2016_05_09_005222_drop_wish_column.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropWishColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn(['wish']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('wish');
+        });
+    }
+}
