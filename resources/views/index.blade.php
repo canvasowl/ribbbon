d3107573f0641ed1bd17ece35a96212fdcdc63e8
Jefry <jefrycruz88@gmail.com>
2016-05-29 14:12:33 -0400

    Home page markup and style changes for header and hero area

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,39 @@
-@extends('templates.outs.auth')
+@extends('templates.outs.home')
 
 @section('content')
-	<a href="/register">register</a>
-	<a href="/login">login</a>
+    {{-- HEADER--}}
+	<div class="hug hug-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <a href="/" class="pull-left"><img src="{{ \App\Helpers\Helpers::logoUrl() }}" alt="Ribbbon"></a>
+                    <a href="/login" class="btn btn-primary btn-line pull-right login">Login</a>
+                    <a href="/register" class="btn btn-primary btn-line pull-right register">Register</a>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+	</div>
+
+    {{-- HEREO SECTIO--}}
+    <div class="hug hug-hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="left-side">
+                        <h1>Introducing Ribbbon 2.0</h1>
+                        <h2>An open source project management system.</h2>
+                        <a href="/register" class="btn btn-special">GET STARTED</a>
+                    </div>
+                    <div class="right-side">
+                        <img class="mascot" src="{{ asset('assets/img/mascot_left.png')  }}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
 @stop
