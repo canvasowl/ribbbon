8ea3039b2b218bc84c85e959e392e48880faa5f6
Jefry <jefrycruz88@gmail.com>
2016-06-23 19:46:58 -0400

    Slight change on welcome email comment

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -91,10 +91,10 @@ class UsersController extends BaseController {
 
 		$user->save();	
 
-//		if ( Auth::attempt(array('email' => $email, 'password' => $password)) ) {
+		if ( Auth::attempt(array('email' => $email, 'password' => $password)) ) {
 //			Helpers::sendWelcomeMail();
-//			return Redirect::to('hud');
-//		}
+			return Redirect::to('hud');
+		}
 
 		return Redirect::back()->withErrors($validator);
 	}	
